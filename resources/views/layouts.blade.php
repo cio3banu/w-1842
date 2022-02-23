870cc21c3564545b8923dbd8497890ba5e9b80a9
ciobanu andrei <cio3banu@gmail.com>
2022-02-23 11:09:20 +0200

    error

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -19,7 +19,24 @@
 
     </ul>
 
-    @yield('content')
+<div class="container">
+
+    @if (count($errors)>0)
+
+    <div class="alert alert-danger" role="alert">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+
+    </ul>
+    </div>
+
+    @endif
+
+        @yield('content')
+
+      </div>
 
 </body>
 </html>
